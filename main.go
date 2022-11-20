3360822e59d254956e3efb049df469c6b91e2730
Lucas Aster <asterlc102@gmail.com>
2022-11-20 15:05:21 -0300

    add

--- main.go
+++ main.go
@@ -1,19 +1,76 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
+
+func cnPostgres() *sql.DB {
+	// user, dbname, password, host, ssl(true/false)
+	con := "user=administrator dbname=produtos password=pass123456 host=localhost sslmode=disable"
+	db, error := sql.Open("postgres", con)
+
+	if error != nil {
+		panic(error.Error())
+	} else {
+		return db
+	}
+}
+
+type Produto struct {
+	id              int
+	Nome, Descricao string
+	Quantidade      int
+	Preco           float64
+}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	fmt.Println("Iniciando na porta 8080")
+	fmt.Println("Iniciando na porta 9080")
+	cnPostgres()
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8080", nil)
+	error := http.ListenAndServe(":9080", nil)
+	if error != nil {
+		panic(error.Error())
+	}
 }
 
 func index(writer http.ResponseWriter, request *http.Request) {
-	temp.ExecuteTemplate(writer, "Index", nil)
+	db := cnPostgres()
+	row, error := db.Query("select * from produtos")
+	if error != nil {
+		panic(error.Error())
+	}
+	p := Produto{}
+	produtos := []Produto{}
+
+	for row.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err := row.Scan(&id, &nome, &descricao, &quantidade, &preco)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Quantidade = quantidade
+		p.Preco = preco
+
+		produtos = append(produtos, p)
+		fmt.Println(produtos)
+
+	}
+
+	temp.ExecuteTemplate(writer, "Index", produtos)
+	defer db.Close()
+
 }
