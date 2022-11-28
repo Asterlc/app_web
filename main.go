6ba3a4a4641aae914e78a195e07182402601cc16
Lucas Aster <asterlc102@gmail.com>
2022-11-27 22:18:45 -0300

    mudar essa criação de banco para o docker futuramente, aprendendo

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"app_web/db"
 	"app_web/routes"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 // CRIAR TABELA: create table produtos(nome varchar, descricao varchar, quantidade integer, preco decimal)
 func main() {
+	db := db.Connect()
+	sqlRes, err := db.Exec(fmt.Sprintf("create table if not exists produtos(nome varchar, descricao varchar, quantidade integer, preco decimal)"))
+	if err != nil {
+		log.Fatalln(err.Error())
+	} else {
+		log.Println(sqlRes)
+	}
+	defer db.Close()
 	fmt.Println("http://localhost:9080")
 	routes.Routes()
 
